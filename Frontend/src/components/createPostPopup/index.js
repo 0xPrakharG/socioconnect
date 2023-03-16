0afe9ec443f2238ee2860a95eb8c097fb35cba84
0xPrakharG <prakhargoyal.3014@gmail.com>
2023-03-16 23:24:34 +0530

    Create Post popup inserted react emoji picker.

--- Frontend/src/components/createPostPopup/index.js
+++ Frontend/src/components/createPostPopup/index.js
@@ -4,6 +4,7 @@ import Picker from 'emoji-picker-react'
 export default function CreatePostPopup({ user }) {
   const [text, setText] = useState("");
   const [showPrev, setShowPrev] = useState(false);
+  const [picker, setPicker] = useState(false);
   return (
     <div className="bg-blur absolute top-0 left-0 z-10 h-[100%] w-[100%]">
       <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] bg-primary shadow-[0_12px_20px_0] shadow-shadow-1 min-h-[220px] w-[500px] rounded-[5px] postBox">
@@ -35,12 +36,18 @@ export default function CreatePostPopup({ user }) {
           className="relative border-none resize-none p-[5px_15px] mb-[10px] w-[470px] ml-[1px] text-[24px] bg-primary text-primary-color min-h--[90px] overflow-y-visible post_input"></textarea>
         </div>
         )}
-        <div className="post_emojis_wrap">
-          <div className="comment_emoji_picker remove">
+        <div className="relative flex justify-between p-[10px_15px] items-end post_emojis_wrap">
+          {picker && (
+          <div className="absolute right-[-21.5rem] bottom-[-14rem] comment_emoji_picker remove">
             <Picker />
           </div>
+          )}
+          <img src="../../../icons/colorful.png" className="w-[39px] cursor-pointer" alt="" />
+          <i className="hover:invert-[80%] emoji_icon_large"></i>
         </div>
       </div>
     </div>
   )
 }
+
+
