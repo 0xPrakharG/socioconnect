efbaadb9cc460d7f28bb05cb80a7a1bc55329239
0xPrakharG <prakhargoyal.3014@gmail.com>
2023-03-23 17:21:48 +0530

    Post Created

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { register, activateAccount, login, auth, sendVerification, findUser, sendResetPasswordCode, validateResetCode, changePassword } = require( "../controllers/user" );
+const { register, activateAccount, login, auth, sendVerification, findUser, sendResetPasswordCode, validateResetCode, changePassword, getProfile } = require( "../controllers/user" );
 const { authUser } = require( "../middleware/auth" );
 
 const router = express.Router();
@@ -12,6 +12,7 @@ router.post("/findUser", findUser);
 router.post("/sendResetPasswordCode", sendResetPasswordCode);
 router.post("/validateResetCode", validateResetCode);
 router.post("/changePassword", changePassword);
+router.get("/getProfile/:username", getProfile);
 
 
 module.exports = router;
--- Frontend/src/pages/home/index.js
+++ Frontend/src/pages/home/index.js
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux";
 import CreatePost from "../../components/createPost";
 import Header from "../../components/header";
@@ -5,23 +6,34 @@ import LeftHome from "../../components/home/left";
 import RightHome from "../../components/home/right";
 import SendVerification from "../../components/home/sendVerification";
 import Stories from "../../components/home/stories";
+import Post from "../../components/post";
 import "./styles.css";
 
-export default function Home({ setVisible }) {
+export default function Home({ setVisible, posts }) {
   const { user } = useSelector((state) => ({...state}));
+  const middle = useRef(null);
+  const [height, setHeight] = useState();
+  useEffect(() => {
+    setHeight(middle.current.clientHeight);
+  }, [])
   return (
-    <div>
-    <div className="min-h-[100vh] bg-secondary grid grid-cols-3">
+    <div className="home" style={{height: `${height+150}px`}}>
       <Header />
       <LeftHome user={user} />
-    <div className="absolute mt-[75px] left-[50%] translate-x-[-50%] home_middle">
+    <div className="absolute mt-[75px] left-[50%] translate-x-[-50%] home_middle" ref={middle}>
       <Stories />
       {
         user.verified === false &&
       <SendVerification user={user} />
       }
       <CreatePost user={user} setVisible={setVisible} />
-    </div>
+      <div className="posts">
+      {
+        posts.map((post) => (
+          <Post key={post._id} post={post} user={user} />
+        ))
+      }
+      </div>
     </div>
       <RightHome user={user} />
     </div>
